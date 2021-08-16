1646e7b4dae9012a258cc735a0097042baa50f11
Luis Guilherme Vieira Neves <luisguilhermevneves@gmail.com>
2021-08-16 17:58:07 -0300

    Adição da segunda função para trabalhar com o soros e as entradas + explicações sobre cada trecho do código

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,10 @@
-let divInferior = document.querySelector('.inferior');
-
-// Capital da conta - A cada win vai ser alterado
+// Capital da conta - onde cada win vai ser somado ou subtraido a ela
 let capital = document.querySelector('#capital');
 capital.innerHTML = 300;
 
+
+// Onde estão acopladas as minhas variaveis de entrada e soros
+let divInferior = document.querySelector('.inferior');
 
 
 // Declaração das entradas e soros
@@ -93,39 +94,55 @@ sorosDoze = parseFloat(sorosDoze);
 
 
 
+// Esse é o meu "mini" banco de dados onde fica aparecendo as ordens dadas do usuário
 let ordensDB = document.querySelector('.ordens');
 
+
+
 /*
-    - Esse addEventListener vai escutar e parar o disparo do evento, e invocar a função checaLoss() e o checaWin()
-    - Eu preciso disso para checar quantas vezes o usuário clicou no botão para eu fazer a lógica do gerenciamento
-    - O contador vai ser o espião que vai ter o valor [0], toda vez que o usuário clicar no loss ou no botão, 
-    o contador vai ser incrementado, meu objetivo é que o contador finalize com 2 dois cliques, então a sessão
-    será finalizada.
+    - Esses são responsaveis por capturar o clique nas caixas WIN E LOSS e fazer com que o meu contadorWin
+    e contadorLoss sejam incrementados a cada clique, graças a eles eu consigo  fazer a lógica nas funções 
 */
+
 let contadorWin = 0;
 let contadorLoss = 0;
-
 
 let loss = document.querySelector('#loss');
 loss.addEventListener('click',function(event){
     event.preventDefault();
-    contadorLoss++
-    checaEntradas();
+    contadorLoss++;
+    primeiraEntrada();
+    segundaEntrada();
 });
 
 
 let win = document.querySelector('#win');
 win.addEventListener('click',function(event){
     event.preventDefault();
-    contadorWin++
-    checaEntradas();
+    contadorWin++;
+    primeiraEntrada();
+    segundaEntrada();
 });
 
 
 /*
-    A cada win o contadorWIn vai ser incrementado e atingir o objetivo final que é 2 wins
+    1. Se contadorWin for igual a um clique E contadorLOSS não tiver nenhum clique
+        - Eu vou adicionar o lucro da primeira entrada 2.75 = 1.93
+        - E vou mandar alterar na minha div que está meu capital
+
+    2. Se contadorWin tiver dois cliques e contadorLOSS não teve nenhum, isso significa que batemos a meta
+        - Então eu vou adicionar ao meu capital o lucro da mão de soros
+
+    3. Se o contadoWIn tiver um clique e contalosLOSS tiver um clique, então houve empate e tivemos prejuizo
+        - Então como a 1 mão já foi somada na lógica de cima, eu só preciso retirar o valor do sorosUM
+
+    4. Se o contadorWin tiver nenhum clique e o contadosLOSS tiver um clique, isso significa que já tomamos
+    prejuizo de cara, então tiramos o valor da primeiraEntrada
+
+    5. Por fim, invoco a 2 função para dar conta caso haja o 1 loss e assim vou indo fazer até a 12 função
+    onde acaba o gerenciamento
 */
-function checaEntradas(){
+function primeiraEntrada(){
     if(contadorWin === 1 && contadorLoss === 0){
         ordensDB.innerHTML += `<p>Sessão com ${contadorWin} Win finalizada ! + 1.93</p> <br>`;
         capital.innerHTML = (parseFloat(capital.innerHTML) + entradaUm).toFixed(2);
@@ -133,65 +150,84 @@ function checaEntradas(){
         capital.style.fontWeight = "bold";
     }
     if(contadorWin === 2 && contadorLoss === 0){
-        ordensDB.innerHTML += `<p>Sessão com ${contadorWin} Win Finalizada ! + 3.27</p> <br>`;
+        ordensDB.innerHTML += `<p>Sessão com ${contadorWin} wins META DO DIA finalizada ! + 3.27</p> <br>`;
         capital.innerHTML = (parseFloat(capital.innerHTML) + sorosUm).toFixed(2);
+
+    }
+    if(contadorWin === 1 && contadorLoss === 1){
+        ordensDB.innerHTML += `<p>Sessão com ${contadorWin} Win e ${contadorLoss} loss Finalizada ! - 4.68</p> <br>`;
+        capital.innerHTML = (parseFloat(capital.innerHTML) - sorosUm ).toFixed(2);
     }
 
-    /*Caso dê sequencia de varias derrotas */
+    //Caso tenha dado loss vou invocar a segunda função
     if(contadorWin === 0 && contadorLoss === 1){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -2,75</p> <br>`;
+        ordensDB.innerHTML += `<p>Sessão com ${contadorLoss} loss STOP LOSS BATIDO ! - 2.75</p> <br>`;
         capital.innerHTML = (parseFloat(capital.innerHTML) - entradaUm).toFixed(2);
         capital.style.color = "rgb(255, 66, 66)";
         capital.style.fontWeight = "bold";
     }
-    if(contadorWin === 0 && contadorLoss === 2){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -3,85</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDois).toFixed(2);
-        
-    }
-    if(contadorWin === 0 && contadorLoss === 3){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -2,65</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaTres).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 4){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -6,46</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaQuatro).toFixed(2);
-    }
-    if(contadorWin == 0 && contadorLoss == 5){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -9,05</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaCinco).toFixed(2);
+    segundaEntrada();
     
-    }
-    if(contadorWin === 0 && contadorLoss === 6){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -9,92</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaSeis).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 7){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -16,64</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaSete).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 8){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -23,31</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaOito).toFixed(2);
-        capital.style.color = "rgb(255, 46, 36)";
-    }
-    if(contadorWin === 0 && contadorLoss === 9){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -29,90</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaNove).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 10){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -44,62</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDez).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 11){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -62,49</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaOnze).toFixed(2);
-    }
-    if(contadorWin === 0 && contadorLoss === 12){
-        ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -87,51</p> <br>`;
-        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDoze).toFixed(2);
-    }
-    
-
 }
+function segundaEntrada(){
+    if(contadorLoss === 2 && contadorWin === 0){
+        ordensDB.innerHTML += `<p>Sessão com ${contadorLoss} loss STOP LOSS BATIDO ! - 3.85</p> <br>`;
+        capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDois).toFixed(2);
+    }
+}
+
+
+/*
+    function checaEntradas(){
+
+
+        if(contadorWin === 0 && contadorLoss === 2){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -3,85</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDois).toFixed(2);
+            
+        }
+        if(contadorWin === 0 && contadorLoss === 3){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -2,65</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaTres).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 4){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -6,46</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaQuatro).toFixed(2);
+        }
+        if(contadorWin == 0 && contadorLoss == 5){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -9,05</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaCinco).toFixed(2);
+        
+        }
+        if(contadorWin === 0 && contadorLoss === 6){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -9,92</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaSeis).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 7){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -16,64</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaSete).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 8){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -23,31</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaOito).toFixed(2);
+            capital.style.color = "rgb(255, 46, 36)";
+        }
+        if(contadorWin === 0 && contadorLoss === 9){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -29,90</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaNove).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 10){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -44,62</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDez).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 11){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -62,49</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaOnze).toFixed(2);
+        }
+        if(contadorWin === 0 && contadorLoss === 12){
+            ordensDB.innerHTML += `<p>Sessão finalizada com ${contadorLoss} Loss ! -87,51</p> <br>`;
+            capital.innerHTML = (parseFloat(capital.innerHTML) - entradaDoze).toFixed(2);
+        }
+*/
+
 
